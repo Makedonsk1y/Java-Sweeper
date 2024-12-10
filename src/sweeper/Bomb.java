cd6b7ddfeaf0eb752c1a0e87c31f1ba39bc1a9b7
Makedonsk1y <alexisvlasov2002@gmail.com>
2024-12-10 15:07:34 +0700

    add increment for numbers around bomb

--- src/sweeper/Bomb.java
+++ src/sweeper/Bomb.java
@@ -23,6 +23,15 @@ class Bomb {
     private void placeBomb(){
         Coord coord = Ranges.getRandomCoord();
         bombMap.set(coord,Box.BOMB);
+        incNumbersAroundBomb(coord);
+    }
+
+    private void incNumbersAroundBomb (Coord coord){
+        for (Coord around : Ranges.getCoordsAround(coord)){
+            if(Box.BOMB != bombMap.get(around)){
+                bombMap.set(around,bombMap.get(around).getNextNumberBox());
+            }
+        }
     }
 }
 
